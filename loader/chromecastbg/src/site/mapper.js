84ef0d437cf3ab21b9d17cb6ec7587c2fd548194
Desmaisons <desmaisons_david@hotmail.com>
2020-09-29 11:28:41 -0300

    Simplifying json output: no need for provider

--- loader/chromecastbg/src/site/mapper.js
+++ loader/chromecastbg/src/site/mapper.js
@@ -1,12 +1,10 @@
 const { Location } = require("../entity/Location");
 const {checkResource} = require("../utils/checkResource");
 
-const provider = "chromeCast";
-
 async function mapper({url, name, photographer, location: rawLocation}){
   const success = await checkResource(url, "image/*");
   const location = rawLocation?  new Location({ title: rawLocation}) : null;
-  return success ? {url, name, photographer, provider, location} : null;
+  return success ? {url, name, photographer, location} : null;
 }
 
 module.exports = {
