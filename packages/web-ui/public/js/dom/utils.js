6f404c0190494f99e04605bc245104bca8286a39
David Desmaisons <desmaisons_david@hotmail.com>
2021-07-08 01:31:17 -0300

    secure request full screen

--- packages/web-ui/public/js/dom/utils.js
+++ packages/web-ui/public/js/dom/utils.js
@@ -1,20 +1,30 @@
-function requestFullScreen() {
+async function requestFullScreen() {
+    try {
+        return await requestFullScreenUnsafe();
+    } catch {
+        return false;
+    }
+}
+
+async function requestFullScreenUnsafe() {
     const {
         document: {
             documentElement
         }
     } = window;
     if (documentElement.requestFullscreen) {
-        documentElement.requestFullscreen();
-        return;
+        await documentElement.requestFullscreen();
+        return true;
     }
     if (documentElement.mozRequestFullScreen) {
-        documentElement.mozRequestFullScreen();
-        return;
+        await documentElement.mozRequestFullScreen();
+        return true;
     }
-    if (documentElement.webkitRequestFullScreen) {
-        documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    if (!documentElement.webkitRequestFullScreen) {
+        return false;
     }
+    await documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    return true;
 }
 
 function loadImage(url) {
